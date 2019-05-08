3a91f4980fd2deb5c82fd367605d609ba400bf4f
lubien <lubien1996@gmail.com>
2019-05-07 21:58:55 -0300

    Implementa e testa rota de importação de CSV

---
+++ server/controllers/students/FromCsv.js
@@ -0,0 +1,80 @@
+const fs = require('fs')
+const utils = require('../../utils')
+
+module.exports = async function studentsFromCsv(ctx) {
+  const filePath =
+    ctx.request.files && ctx.request.files.csv && ctx.request.files.csv.path
+
+  if (!filePath) {
+    ctx.status = 400
+    ctx.body = { message: `Missing 'csv' field` }
+    return
+  }
+
+  const csv = fs.readFileSync(filePath, 'utf8')
+
+  if (!csv) {
+    ctx.status = 400
+    ctx.body = { message: `Failed to upload file` }
+    return
+  }
+
+  const validation = validate(csv)
+
+  if (typeof validation === 'string') {
+    ctx.status = 400
+    ctx.body = { message: validation }
+    return
+  }
+
+  const data = utils.parseCsv(csv)
+  const digested = utils.digestSigaaData(data)
+  await utils.batchUpdateStudents(digested)
+
+  ctx.status = 201
+  ctx.body = {
+    count: digested.length
+  }
+}
+
+const validHeader =
+  'Matrícula,AnoIngresso,Nome,CPF,DataNascimento,NomeMae,Municipio,Curso,Status'
+
+/**
+ * Read a CSV from SIGAA and verify if it's valid (return true) or
+ * return a message of what's wrong.
+ *
+ * @param {string} csv - CSV from SIGAA
+ * @return {(string|boolean)} True for valid or string for error message
+ *
+ * @example
+ *
+ *     const csv = `Matrícula,AnoIngresso,Nome,CPF,DataNascimento,NomeMae,Municipio,Curso,Status
+ * 201704940001,2017,FELIPE SOUZA FERREIRA,111.111.111-11,1/29/1995,VITORIA DIAS ROCHA,Belém,CIENCIA DA COMPUTACAO,FORMANDO`
+ *     validate(csv)
+ */
+function validate(csv) {
+  try {
+    const lines = csv.split('\n')
+
+    if (lines.length < 2) {
+      return 'Invalid length'
+    }
+
+    if (lines[0] !== validHeader) {
+      return 'Invalid header'
+    }
+
+    const rightNumberOfColumns = lines.every(
+      line => line.split(',').length === 9
+    )
+
+    if (!rightNumberOfColumns) {
+      return 'Invalid number of columns'
+    }
+
+    return true
+  } catch (err) {
+    return 'Invalid file'
+  }
+}
---
+++ test/server/controllers/students.spec.js
@@ -0,0 +1,191 @@
+/**
+ * @jest-environment node
+ */
+
+const path = require('path')
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+chai.use(chaiHttp)
+
+const server = require('../../../server')
+const db = require('../../../server/db')
+const Student = require('../../../server/models/Student')
+
+jest.useFakeTimers()
+
+/**
+ * Helper function to get fixtures path
+ *
+ * @param {string} name - A file name from test/server/fixtures folder
+ * @return {string} File full path
+ *
+ * @example
+ *
+ *     sigaaCsvFixture('valid.csv')
+ */
+function sigaaCsvFixture(name) {
+  return path.join(__dirname, '../fixtures/sigaa', name)
+}
+
+describe('/api/students', () => {
+  beforeEach(async () => {
+    await db.knex.migrate.rollback()
+    await db.knex.migrate.latest()
+    await db.knex.seed.run()
+  }, 100000)
+
+  test('POST /from-csv', async done => {
+    /**
+     * Find a student by it's registrationNumber
+     *
+     * @param {object[]} students - Object of student from the database
+     * @param {string} registrationNumber - The wanted student's registrationNumber
+     * @return {object} The student
+     *
+     * @example
+     *
+     *     const students = [{ registrationNumber: '123', name: 'test user' }]
+     *     findByRegistrationNumber(students, '123')
+     */
+    function findByRegistrationNumber(students, registrationNumber) {
+      return students.find(u => u.registrationNumber === registrationNumber)
+    }
+
+    const res = await chai
+      .request(server.listen())
+      .post('/api/students/from-csv')
+      .attach('csv', sigaaCsvFixture('valid.csv'), 'export.csv')
+      .type('form')
+
+    expect(res.status).toEqual(201)
+    expect(res.type).toEqual('application/json')
+    expect(res.body).toBeDefined()
+    expect(res.body.count).toEqual(7)
+
+    const studentsUpdated = (await Student.fetchAll()).toJSON()
+
+    const [felipe, laura, jose, enzo, kauan, eduardo, julian] = [
+      '201704940001',
+      '201304940002',
+      '200504940003',
+      '201104940004',
+      '200604940005',
+      '201804940006',
+      '199604940007'
+    ].map(registrationNumber =>
+      findByRegistrationNumber(studentsUpdated, registrationNumber)
+    )
+
+    expect(studentsUpdated.length).toEqual(7)
+
+    expect(felipe.isActive).toBeTruthy()
+    expect(felipe.isForming).toBeTruthy()
+    expect(felipe.isGraduating).toBeFalsy()
+    expect(felipe.isConcluding).toBeFalsy()
+    expect(felipe.cancelled).toBeFalsy()
+
+    expect(laura.isActive).toBeFalsy()
+    expect(laura.isForming).toBeFalsy()
+    expect(laura.isGraduating).toBeFalsy()
+    expect(laura.isConcluding).toBeTruthy()
+    expect(laura.cancelled).toBeFalsy()
+
+    expect(jose.isActive).toBeFalsy()
+    expect(jose.isForming).toBeFalsy()
+    expect(jose.isGraduating).toBeFalsy()
+    expect(jose.isConcluding).toBeTruthy()
+    expect(jose.cancelled).toBeFalsy()
+
+    expect(enzo.isActive).toBeTruthy()
+    expect(enzo.isGraduating).toBeFalsy()
+    expect(enzo.isGraduating).toBeFalsy()
+    expect(enzo.isConcluding).toBeFalsy()
+    expect(enzo.cancelled).toBeFalsy()
+
+    expect(kauan.isActive).toBeFalsy()
+    expect(kauan.isGraduating).toBeFalsy()
+    expect(kauan.isGraduating).toBeFalsy()
+    expect(kauan.isConcluding).toBeFalsy()
+    expect(kauan.cancelled).toBeTruthy()
+
+    expect(eduardo.isActive).toBeTruthy()
+    expect(eduardo.isGraduating).toBeFalsy()
+    expect(eduardo.isGraduating).toBeFalsy()
+    expect(eduardo.isConcluding).toBeFalsy()
+    expect(eduardo.cancelled).toBeFalsy()
+
+    expect(julian.isActive).toBeFalsy()
+    expect(julian.isGraduating).toBeFalsy()
+    expect(julian.isGraduating).toBeFalsy()
+    expect(julian.isConcluding).toBeTruthy()
+    expect(julian.cancelled).toBeFalsy()
+
+    done()
+  })
+
+  test('POST /from-csv missing csv field', async done => {
+    const res = await chai
+      .request(server.listen())
+      .post('/api/students/from-csv')
+      .type('form')
+
+    debugger
+
+    expect(res.status).toEqual(400)
+    expect(res.type).toEqual('application/json')
+    expect(res.body).toBeDefined()
+    done()
+  })
+
+  test('POST /from-csv invalid CSV length', async done => {
+    const res = await chai
+      .request(server.listen())
+      .post('/api/students/from-csv')
+      .attach('csv', sigaaCsvFixture('wrong-length.csv'), 'export.csv')
+      .type('form')
+
+    expect(res.status).toEqual(400)
+    expect(res.type).toEqual('application/json')
+    expect(res.body).toBeDefined()
+    done()
+  })
+
+  test('POST /from-csv invalid CSV headers', async done => {
+    const res = await chai
+      .request(server.listen())
+      .post('/api/students/from-csv')
+      .attach('csv', sigaaCsvFixture('wrong-headers.csv'), 'export.csv')
+      .type('form')
+
+    expect(res.status).toEqual(400)
+    expect(res.type).toEqual('application/json')
+    expect(res.body).toBeDefined()
+    done()
+  })
+
+  test('POST /from-csv invalid CSV column number', async done => {
+    const res = await chai
+      .request(server.listen())
+      .post('/api/students/from-csv')
+      .attach('csv', sigaaCsvFixture('wrong-col-number.csv'), 'export.csv')
+      .type('form')
+
+    expect(res.status).toEqual(400)
+    expect(res.type).toEqual('application/json')
+    expect(res.body).toBeDefined()
+    done()
+  })
+
+  test('POST /from-csv invalid not CSV ', async done => {
+    const res = await chai
+      .request(server.listen())
+      .post('/api/students/from-csv')
+      .attach('csv', sigaaCsvFixture('wrong-invalid-csv.csv'), 'export.csv')
+      .type('form')
+
+    expect(res.status).toEqual(400)
+    expect(res.type).toEqual('application/json')
+    expect(res.body).toBeDefined()
+    done()
+  })
+})
