059ab0170a6686d9dc99b3892b3ceb11e8fb788c
Lakshamana <guitrompa1@gmail.com>
2019-05-10 13:02:36 -0300

    Inicia a implementação da página de alunos ativos
    > Mockada. Requer integração com o backend
    > Possivelmente terá o campo de pesquisa substituido pelo componente de campos de pesquisa (ver PR #13)

--- pages/activeStudents.vue
+++ pages/activeStudents.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="container">
-    <div v-if="courseTag" class="container">
+    <div v-if="courseTag">
       <div class="columns">
-        <div class="column is-centered is-half">
-          <b-input
-            v-model="studentSearch"
-            placeholder="Buscar Aluno"
-            rounded
-          ></b-input>
+        <div class="column is-centered is-half is-offset-one-quarter">
+          <div class="control has-icons-left">
+            <b-input
+              v-model="studentSearch"
+              placeholder="Buscar aluno"
+              type="search"
+              icon="magnify"
+              rounded
+            ></b-input>
+          </div>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column is-centered is-half is-offset-one-quarter">
+          <b-table :data="searchStudentResult" :columns="columns"></b-table>
         </div>
       </div>
     </div>
@@ -22,10 +31,54 @@ export default {
   middleware: 'auth',
 
   data: () => ({
-    studentSearch: ''
+    studentSearch: '',
+    students: [
+      {
+        number: '111122223330',
+        name: 'Student 1',
+        email: 'student1@ufpa.br'
+      },
+      {
+        number: '111122223331',
+        name: 'Student 2',
+        email: 'student2@ufpa.br'
+      },
+      {
+        number: '111122223332',
+        name: 'Student 2',
+        email: 'student2@ufpa.br'
+      },
+      {
+        number: '111122223333',
+        name: 'Student 3',
+        email: 'student3@ufpa.br'
+      }
+    ],
+
+    columns: [
+      {
+        field: 'number',
+        label: 'Number',
+        width: '40'
+      },
+      {
+        field: 'name',
+        label: 'Name'
+      },
+      {
+        field: 'email',
+        label: 'Email'
+      }
+    ]
   }),
 
   computed: {
+    searchStudentResult() {
+      return this.students.filter(st => {
+        return st.name.indexOf(this.studentSearch) !== -1
+      })
+    },
+
     ...mapState({
       courseTag: state => state.courseTag
     })
@@ -33,4 +86,8 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  margin: 20px auto 50px auto;
+}
+</style>
