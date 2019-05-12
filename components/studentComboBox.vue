7d5e8b0ee5df07b218e7f26767f15b1948d3cd93
urielfcampos <urielfcampos95@gmail.com>
2019-05-12 18:03:05 -0300

    Iniciado desenvolvimento do combobox

---
+++ components/studentComboBox.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title">
+        {{ student.name }}
+      </p>
+      <a href="#" class="card-header-icon" aria-label="more options">
+        <span class="icon">
+          <i class="fas fa-angle-down" aria-hidden="true"></i>
+        </span>
+      </a>
+    </header>
+    <div class="card-content">
+      <div class="content">
+        <strong>Nome</strong> : {{ student.name }} <br />
+        <strong>Matricula</strong> : {{ student.registrationNumber }} <br />
+        <strong>E-mail</strong> : {{ student.email }} <br />
+        <strong>Status</strong> : {{ displayStatus(student) }} <br />
+        <strong>Pendências</strong> : {{ student.pendencies }} <br />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StudentComboBox',
+  props: {
+    student: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  methods: {
+    displayStatus() {
+      // TODO FUNCAO PARA DIZER O STATUS DO ALUNO
+    }
+  }
+}
+</script>
+
+<style></style>
