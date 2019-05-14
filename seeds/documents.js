961cd159c42a64efd2a3b17c73e6d589e8097ef6
Lucas Souza <lucassouzaufpa@gmail.com>
2019-05-14 10:35:38 -0300

    Cria seed para documents

---
+++ seeds/documents.js
@@ -0,0 +1,31 @@
+const User = require('../server/models/Document')
+
+exports.seed = async function(knex, Promise) {
+  // Deletes ALL existing entries
+  await knex('documents').del()
+
+  const data = [
+    {
+      studentID: 1,
+      type: 1,
+      URL: 'url/file1'
+    },
+    {
+      studentID: 1,
+      type: 2,
+      URL: 'url/file2'
+    },
+    {
+      studentID: 2,
+      type: 3,
+      URL: 'url/file3'
+    },
+    {
+      studentID: 20,
+      type: 1,
+      URL: 'url/file4'
+    }
+  ]
+
+  return Promise.all(data.map(user => User.forge(user).save()))
+}
