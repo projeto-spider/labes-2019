f48a4d5566fe102864b8574aeee8b29aae1d6248
Lucas Souza <lucassouzaufpa@gmail.com>
2019-05-14 21:29:13 -0300

    Insere erro 404 para rotas desconhecidas da API

---
+++ test/server/router.spec.js
@@ -0,0 +1,49 @@
+/**
+ * @jest-environment node
+ */
+
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+chai.use(chaiHttp)
+
+const db = require('../../server/db')
+const server = require('../../server')
+const errors = require('../../shared/errors')
+
+jest.useFakeTimers()
+
+describe('/api', () => {
+  beforeEach(async done => {
+    await db.knex.migrate.rollback()
+    await db.knex.migrate.latest()
+    await db.knex.seed.run()
+    done()
+  }, 100000)
+  test('GET /', async done => {
+    const res = await chai.request(server.listen()).get('/api/')
+    expect(res.status).toEqual(404)
+    expect(res.type).toEqual('application/json')
+    expect(res.body.code).toEqual(errors.NOT_FOUND_ROUTE)
+    done()
+  })
+  test('GET /invalid', async done => {
+    const res = await chai.request(server.listen()).get('/api/invalid')
+    expect(res.status).toEqual(404)
+    expect(res.type).toEqual('application/json')
+    expect(res.body.code).toEqual(errors.NOT_FOUND_ROUTE)
+    done()
+  })
+  test('GET /[students|users]', async done => {
+    const resStudents = await chai.request(server.listen()).get('/api/students')
+    expect(resStudents.status).toEqual(200)
+    expect(resStudents.type).toEqual('application/json')
+    expect(resStudents.body).toBeDefined()
+    expect(resStudents.body !== { code: errors.NOT_FOUND_ROUTE }).toBeTruthy()
+    const resUsers = await chai.request(server.listen()).get('/api/users')
+    expect(resUsers.status).toEqual(200)
+    expect(resUsers.type).toEqual('application/json')
+    expect(resUsers.body).toBeDefined()
+    expect(resUsers.body !== { code: errors.NOT_FOUND_ROUTE }).toBeTruthy()
+    done()
+  })
+})
