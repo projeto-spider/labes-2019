7b03a776b305aa508ebf3a53dfb41118bb9dcac8
Lucas Souza <lucassouzaufpa@gmail.com>
2019-05-15 20:59:21 -0300

    Inclui Upload para documentos do Aluno

--- server/controllers/documents/Upload.js
+++ server/controllers/documents/Upload.js
@@ -1,13 +1,18 @@
 const fs = require('fs')
-const os = require('os')
 const path = require('path')
 const errors = require('../../../shared/errors')
+const enumi = require('../../../shared/enum')
+
+const Students = require('../../models/Student')
+const Documents = require('../../models/Document')
 
 module.exports = async function uploadDocument(ctx) {
   // reference to documentTypes '../../../shared/enum'
+  const { documentType } = ctx.request.body
+  const { studentID } = ctx.params
+
   const filePath =
     ctx.request.files && ctx.request.files.file && ctx.request.files.file.path
-  const { documentType } = ctx.request.body
 
   if (documentType === undefined || !['1', '2', '3'].includes(documentType)) {
     ctx.status = 400
@@ -33,12 +38,49 @@ module.exports = async function uploadDocument(ctx) {
     ctx.body = { code: errors.UPLOAD_FILE_TYPE_INVALID }
     return
   }
-  const file = ctx.request.files.file
-  const reader = fs.createReadStream(file.path)
-  const stream = fs.createWriteStream(
-    path.join(__dirname, '../../../storage', Math.random().toString() + '.pdf')
-  )
-  reader.pipe(stream)
-  ctx.status = 201
-  ctx.body = 'uploading ' + file.name + ' -> ' + stream.path
+
+  const studentFind = await Students.where('id', studentID).fetch()
+  if (studentFind !== null) {
+    const documentFind = await Documents.where('studentID', studentID)
+      .where('type', documentType)
+      .fetch()
+    const urlApi = '/api/students/' + studentID + '/documents/'
+    if (documentFind === null) {
+      const documentCreated = await Documents.forge().save({
+        studentID: studentID,
+        type: documentType,
+        URL: urlApi
+      })
+      documentCreated.save({
+        id: documentCreated.get('id'),
+        URL: urlApi + documentCreated.get('id')
+      })
+    } else {
+      documentFind.save({
+        id: documentFind.get('id'),
+        URL: urlApi + documentFind.get('id')
+      })
+    }
+
+    const file = ctx.request.files.file
+    const reader = fs.createReadStream(file.path)
+    const stream = fs.createWriteStream(
+      path.join(
+        __dirname,
+        '../../../storage/',
+        studentFind.get('registrationNumber') +
+          '-' +
+          enumi.documents[documentType] +
+          '.pdf'
+      )
+    )
+    reader.pipe(stream)
+    ctx.status = 201
+    ctx.body = {
+      count: file.size
+    }
+  } else {
+    ctx.status = 400
+    ctx.body = { code: errors.USER_NOT_FOUND }
+  }
 }
--- server/controllers/documents/index.js
+++ server/controllers/documents/index.js
@@ -1,4 +1,6 @@
 const Show = require('./Show')
 const List = require('./List')
+const View = require('./View')
+const Upload = require('./Upload')
 
-module.exports = { Show, List }
+module.exports = { Show, List, View, Upload }
