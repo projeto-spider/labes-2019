32596e6e7e33b636ef2053c0a6036f406f5c49c7
Lucas Souza <lucassouzaufpa@gmail.com>
2019-05-15 21:08:34 -0300

    Retira require para View

--- server/controllers/documents/index.js
+++ server/controllers/documents/index.js
@@ -1,6 +1,5 @@
 const Show = require('./Show')
 const List = require('./List')
-const View = require('./View')
 const Upload = require('./Upload')
 
-module.exports = { Show, List, View, Upload }
+module.exports = { Show, List, Upload }
