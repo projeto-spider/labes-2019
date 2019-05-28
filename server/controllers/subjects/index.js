dc6fea429ec9bb93a171993d10d92c49850a8fa4
Daniel Santana <daniel.santana.1661@gmail.com>
2019-05-27 22:32:00 -0300

    Index do CRUD de matérias criado

---
+++ server/controllers/subjects/index.js
@@ -0,0 +1,7 @@
+const Create = require('./Create')
+const List = require('./List')
+const Update = require('./Update')
+const Destroy = require('./Delete')
+const Show = require('./Show')
+
+module.exports = { Create, List, Update, Destroy, Show }
