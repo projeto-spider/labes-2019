b3e6799d254efb8150b301ad320dd38515eb2265
Daniel Santana <daniel.santana.1661@gmail.com>
2019-05-28 03:20:16 -0300

    Tratamento pra delete de matéria não existente

--- server/controllers/subjects/Delete.js
+++ server/controllers/subjects/Delete.js
@@ -1,6 +1,14 @@
 const Subject = require('../../models/Subject')
+const errors = require('../../../shared/errors')
 
 module.exports = async function destroySubject(ctx) {
   const { id } = ctx.params
+
+  const studentFind = await Subject.where('id', id).fetch()
+  if (studentFind === null) {
+    ctx.status = 204
+    ctx.body = { code: errors.NOT_FOUND }
+    return
+  }
   ctx.body = await Subject.where({ id }).destroy()
 }
