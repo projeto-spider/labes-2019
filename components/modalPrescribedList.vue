eecfbfc08a4d297635b3e3e48f2e9467e0fea260
quaresmateo <teoian.giovane@gmail.com>
2019-06-07 00:26:59 -0300

    Cria lista de alunos na lista de prescrição

--- components/modalPrescribedList.vue
+++ components/modalPrescribedList.vue
@@ -17,7 +17,22 @@
           ></button>
         </header>
         <section class="modal-card-body">
-          {{ prescribedList }}
+          <!-- {{ studentsData[0].name }} -->
+          <!-- botar um table -->
+          <div class="list is-hoverable">
+            <a
+              v-for="student in studentsData"
+              :key="student.id"
+              class="list-item"
+            >
+              <div class="level">
+                <div class="level-left">{{ student.name }}</div>
+                <div class="level-right">
+                  <span class="tag is-light">{{ student.status }}</span>
+                </div>
+              </div>
+            </a>
+          </div>
         </section>
         <footer class="modal-card-foot">
           <button class="button" @click.stop.prevent="openModal">
@@ -30,13 +45,16 @@
 </template>
 
 <script>
+import { studentStatus } from './mixins/studentStatus'
 export default {
   name: 'ModalPrescribedList',
+
+  mixins: [studentStatus],
 
   data() {
     return {
       isActive: false,
-      prescribedList: ''
+      studentsData: []
     }
   },
 
@@ -46,7 +64,7 @@ export default {
       this.$axios
         .get('/api/students/?prescribed=1')
         .then(res => {
-          this.prescribedList = res.data.prescribed
+          this.studentsData = res.data
         })
         .catch(() => {
           this.$toast.open({
