3264a941189ab35891bc5594c83a7587b64a2643
Lucas Souza <lucassouzaufpa@gmail.com>
2019-06-13 22:41:23 -0300

    Utiliza transação para join de Subjects e Students

--- server/controllers/pendencies/List.js
+++ server/controllers/pendencies/List.js
@@ -1,4 +1,5 @@
 const errors = require('../../../shared/errors')
+const { knex } = require('../../db')
 
 const Students = require('../../models/Student')
 const Pendencies = require('../../models/Pendency')
@@ -13,9 +14,14 @@ module.exports = async function listPendencies(ctx) {
     return
   }
 
-  const subjectsIds = await Pendencies.where({ studentId })
-    .fetchAll()
-    .then(collection => collection.map(pendency => pendency.get('subjectId')))
+  ctx.body = await knex.transaction(async trx => {
+    const subjectsIds = await Pendencies.where({ studentId })
+      .fetchAll({ transacting: trx })
+      .then(collection => collection.map(pendency => pendency.get('subjectId')))
 
-  ctx.body = await Subjects.where('id', 'in', subjectsIds).fetchAll()
+    const subjectsFind = await Subjects.where('id', 'in', subjectsIds).fetchAll(
+      { transacting: trx }
+    )
+    return subjectsFind
+  })
 }
