0f9446ccebd93a9eb4444e893c553b60e60288e2
Daniel Santana <daniel.santana.1661@gmail.com>
2019-06-24 00:58:19 -0300

    Função de criar documentos de tcc criada

--- server/controllers/documents/TccGenerate.js
+++ server/controllers/documents/TccGenerate.js
@@ -22,81 +22,49 @@ module.exports = async function generateAllDocs(ctx) {
   }
 
   const dados = {
-    curso: '',
-    tituloTCC: '',
-    nomeDosAlunos: '',
-    tituloOrientador: '',
-    orientador: '',
-    tituloCoOrientador: '',
-    coOrientador: '',
-    diaDefesa: '',
-    mesDefesa: '',
-    anoDefesa: '',
-    tituloAvaliador1: '',
-    avaliador1: '',
-    tituloAvaliador2: '',
-    avaliador2: '',
-    tituloAvaliador3: '',
-    avaliador3: '',
-    dia: '',
-    mes: '',
-    ano: '',
-    tituloDiretor: '',
-    diretor: '',
-    matricula: '',
-    membroConvidado: '',
-    discente: '',
-    palavrasChave: '',
-    horarioDefesa: '',
-    salaDefesa: '',
-    resumo: '',
-    pessoa: '',
-    tituloPessoa: '',
-    condicao: ''
+    curso: 'curso',
+    tituloTCC: 'titulotcc',
+    nomeDosAlunos: 'nome dos alunos',
+    tituloOrientador: 'tituloorientador',
+    orientador: 'orientador',
+    tituloCoOrientador: 'titulocoorientador',
+    coOrientador: 'coorientador',
+    diaDefesa: 'diadefesa',
+    mesDefesa: 'mesdefesa',
+    anoDefesa: 'anodefesa',
+    tituloAvaliador1: 'tituloavaliador1',
+    avaliador1: 'avaliador1',
+    tituloAvaliador2: 'tituloavaliador2',
+    avaliador2: 'avaliador2',
+    tituloAvaliador3: 'tituloavaliador3',
+    avaliador3: 'avaliador3',
+    dia: 'dia',
+    mes: 'mes',
+    ano: 'ano',
+    tituloDiretor: 'titulodiretor',
+    diretor: 'diretor',
+    matricula: 'matricula',
+    membroConvidado: 'membro convidado',
+    discente: 'discente',
+    palavrasChave: 'palavras chave',
+    horarioDefesa: 'horariodefesa',
+    salaDefesa: 'saladefesa',
+    resumo: 'resumo',
+    pessoa: 'pessoa',
+    tituloPessoa: 'titulopessoa',
+    condicao: 'condicao'
   }
+  // FILL ALL DATA
 
-  const course = studentFind.get('course')
-  dados.curso = ''
-  dados.tituloTCC = 'T.T'
-  dados.nomeDosAlunos = studentFind.get('name')
-  dados.tituloOrientador = 'T.O'
-  dados.orientador = studentFind.get('advisor')
-  dados.tituloCoOrientador = 'T.C.O'
-  dados.coOrientador = 'C.O'
-  dados.diaDefesa = studentFind.get('defenseDate')
-  dados.mesDefesa = studentFind.get('defenseDate')
-  dados.anoDefesa = studentFind.get('defenseDate')
-  dados.tituloAvaliador1 = 'T.A.1'
-  dados.avaliador1 = 'A.1'
-  dados.tituloAvaliador2 = 'T.A.2'
-  dados.avaliador2 = 'A.2'
-  dados.tituloAvaliador3 = 'T.A.3'
-  dados.avaliador3 = 'A.3'
-  const date = new Date()
-  dados.dia = date.getUTCDate()
-  dados.mes = date.getUTCMonth()
-  dados.ano = date.getUTCFullYear()
-  dados.tituloDiretor = 'T.D'
-  dados.diretor = 'D'
-  dados.matricula = studentFind.get('registrationNumber')
-  dados.membroConvidado = 'M.C'
-  dados.discente = studentFind.get('name')
-  dados.palavrasChave = 'Keywords'
-  dados.horarioDefesa = 'H.D'
-  dados.salaDefesa = 'S.D'
-  dados.resumo = 'R'
-  dados.tituloPessoa = 'T.P'
-  dados.condicao = 'C'
-
-  const doc = new PDFDocument()
-  Ata(doc, dados)
-  Cd(doc, dados)
+  let doc = new PDFDocument()
+  doc = Ata(doc, dados)
+  doc = Cd(doc, dados)
   doc.addPage()
-  Certificado(doc, dados)
+  doc = Certificado(doc, dados)
   doc.addPage()
-  Credenciamento(doc, dados)
+  doc = Credenciamento(doc, dados)
   doc.addPage()
-  Divulgacao(doc, dados)
+  doc = Divulgacao(doc, dados)
   doc.end()
 
   ctx.status = 200
