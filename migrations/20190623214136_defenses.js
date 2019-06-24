6ad5b918fb33d88a5e6281e3b6ea246566aed00a
lubien <lubien1996@gmail.com>
2019-06-23 23:11:27 -0300

    Cria modelo de Defense

---
+++ migrations/20190623214136_defenses.js
@@ -0,0 +1,42 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('defenses', table => {
+    table.increments('id').primary()
+    table
+      .enum('course', ['cbcc', 'cbsi'])
+      .notNullable()
+      .defaultTo('cbcc')
+    table.string('registrationNumbers').notNullable()
+    table.string('students').notNullable()
+    table.string('local').notNullable()
+    table.date('date').nullable()
+    table.date('time').nullable()
+    table.string('title').notNullable()
+    table.string('keywords').notNullable()
+    table.text('summary').nullable()
+
+    const people = [
+      { person: 'advisor', mandatory: true },
+      { person: 'coAdvisor', mandatory: false },
+      { person: 'evaluator1', mandatory: true },
+      { person: 'evaluator2', mandatory: true },
+      { person: 'evaluator3', mandatory: false }
+    ]
+    for (const { person, mandatory } of people) {
+      const nullability = mandatory ? 'notNullable' : 'nullable'
+
+      table.string(`${person}Name`)[nullability]() // advisorName
+      table // advisorTitle
+        .enum(`${person}Title`, ['doctor', 'master', 'other'])
+        [nullability]()
+        .defaultTo('other')
+      table // advisorType
+        .enum(`${person}Type`, ['internal', 'external'])
+        [nullability]()
+        .defaultTo('internal')
+    }
+  })
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('defenses')
+}
---
+++ test/server/models/defense.spec.js
@@ -0,0 +1,53 @@
+/**
+ * @jest-environment node
+ */
+
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+chai.use(chaiHttp)
+
+const db = require('../../../server/db')
+const Defense = require('../../../server/models/Defense')
+
+jest.useFakeTimers()
+
+describe('models/Defense', () => {
+  beforeEach(async done => {
+    await db.knex.migrate.rollback()
+    await db.knex.migrate.latest()
+    await db.knex.seed.run()
+    done()
+  }, 100000)
+
+  test('Create a Defense', async done => {
+    const payload = {
+      course: 'cbcc',
+      registrationNumbers: '201704940001, 201304940002',
+      students: 'FELIPE SOUZA FERREIRA, LAURA CARDOSO CASTRO',
+      local: 'Auditório do ICEN',
+      title: 'Fundamentos da Comunicação Analógica',
+      keywords: 'Fundamental, comunicacional, analógico',
+      summary: 'Sumário fundamentacional',
+
+      advisorName: 'Jonathan Joestar',
+      advisorTitle: 'doctor',
+      advisorType: 'internal',
+
+      evaluator1Name: 'Robert E. O. Speedwagon',
+      evaluator1Title: 'doctor',
+      evaluator1Type: 'internal',
+
+      evaluator2Name: 'Narciso Anasui',
+      evaluator2Title: 'master',
+      evaluator2Type: 'external'
+    }
+    const defense = await Defense.forge(payload).save()
+    expect(defense.id).toBeDefined()
+    expect(defense.attributes.registrationNumbers).toEqual(
+      payload.registrationNumbers
+    )
+    expect(defense.attributes.evaluator1Name).toEqual(payload.evaluator1Name)
+    expect(defense.attributes.summary).toEqual(payload.summary)
+    done()
+  })
+})
