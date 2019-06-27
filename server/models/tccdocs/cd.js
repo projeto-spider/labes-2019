b853592cec5ebb1f027762a07290bd7a69864c62
Daniel Santana <daniel.santana.1661@gmail.com>
2019-06-27 00:45:47 -0300

    Correção de alguns espaçamentos no Cd

--- server/models/tccdocs/cd.js
+++ server/models/tccdocs/cd.js
@@ -14,14 +14,10 @@ module.exports = function model(
   doc.addPage({
     layout: 'landscape'
   })
-
-  // Dash recorte
   doc
     .rect(0, 0, 697.14, 348.57)
     .dash(5, { space: 10 })
     .stroke()
-
-  // Header
   doc.image('./server/models/tccdocs/img/logoUFPA.png', 180, 30, {
     align: 'left',
     valign: 'top'
@@ -39,77 +35,59 @@ module.exports = function model(
       align: 'center'
     }
   )
-  // End of Header
-
   doc
     .font('Times-Bold')
     .fontSize(10)
     .text(`${tituloTCC}`, -300, 180, {
       align: 'center'
     })
-
   doc
     .font('Times-Bold')
     .fontSize(10)
     .text(`${nomeDosAlunos}`, -300, 205, {
       align: 'center'
     })
-
   doc
     .fontSize(10)
-    .text(`Orientador(a): ${tituloOrientador}(a) ${orientador}`, 30, 270, {
+    .text(`Orientador(a): ${tituloOrientador}${orientador}`, 30, 270, {
       align: 'left'
     })
-
   if (coOrientador) {
     doc
       .fontSize(10)
-      .text(
-        `Co-Orientador(a): ${tituloCoOrientador}(a) ${coOrientador}`,
-        30,
-        290,
-        {
-          align: 'left'
-        }
-      )
+      .text(`Co-Orientador(a): ${tituloCoOrientador}${coOrientador}`, 30, 290, {
+        align: 'left'
+      })
   }
-
   doc.fontSize(10).text(`Belém, ${ano}`, -300, 330, {
     align: 'center'
   })
-
   doc
     .font('Times-Bold')
     .fontSize(12)
     .text(`${tituloTCC}`, 330, 40, {
       align: 'center'
     })
-
   doc
     .font('Times-Bold')
     .fontSize(10)
     .text(`${nomeDosAlunos}`, 330, 80, {
       align: 'center'
     })
-
-  doc.fontSize(9).text(
-    `Declaro que este CD-ROM contém o trabalho de conclusão corrigido e
-finalizado conforme minha orientação.`, // Manter colado desse jeito, para que não haja indentação no texto.
-    420,
-    150
-  )
-
+  doc
+    .fontSize(9)
+    .text(
+      `Declaro que este CD-ROM contém o trabalho de conclusão corrigido e finalizado conforme minha orientação.`,
+      420,
+      150
+    )
   doc.fontSize(10).text(`____________________________________`, 450, 250)
-
-  doc.fontSize(10).text(`${tituloOrientador}(a) ${orientador}`, 470, 270)
-
-  doc.fontSize(12).text(
-    `Deve ser entregue na secretaria da faculdade, já com a assinatura do seu Orientador(a).
-
-Em caso de rasura, ou quaisquer formas de deterioração deste papel, por favor, trocar na secretaria.`,
-    100,
-    450
-  )
-
-  return doc
+  doc.fontSize(10).text(`${tituloOrientador}${orientador}`, 470, 270)
+  doc
+    .fontSize(12)
+    .text(
+      `Deve ser entregue na secretaria da faculdade, já com a assinatura do seu Orientador(a).\n\nEm caso de rasura, ou quaisquer formas de deterioração deste papel, por favor, trocar na secretaria.`,
+      100,
+      450
+    )
 }
