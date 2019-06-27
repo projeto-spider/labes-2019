f45decf030805f78591ec487b390b14f35336e2c
quaresmateo <teoian.giovane@gmail.com>
2019-06-27 17:09:24 -0300

    Adiciona tccDefense na página home do professor.

--- pages/teacher/home.vue
+++ pages/teacher/home.vue
@@ -1,28 +1,22 @@
 <template>
   <section class="section">
     <div class="container">
-      <h1 class="title">Defesas de TCC pendentes</h1>
-      <ul>
-        <li
-          v-for="(presentation, index) in registeredPresentations"
-          :key="index"
-        >
-          <div class="box" style="width:30rem; margin:2em 0">
-            {{ presentation }}
-          </div>
-        </li>
-      </ul>
+      <tcc-defense></tcc-defense>
     </div>
   </section>
 </template>
 
 <script>
+import TccDefense from '~/pages/tccDefense'
 export default {
   name: 'TeacherHome',
   layout: 'defaultTeacher',
+  components: {
+    TccDefense
+  },
   data() {
     return {
-      registeredPresentations: [1, 2, 3]
+      registeredPresentations: []
     }
   }
 }
