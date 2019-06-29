588a01c5ef6238151f2267a8c43e0328c43af5dd
Lakshamana <guitrompa1@gmail.com>
2019-06-28 23:39:03 -0300

    Adiciona função seletora

---
+++ front/selector.js
@@ -0,0 +1,13 @@
+export default function select(data, getList, defaults = {}) {
+  if (data && typeof data == 'object') {
+    const selected = {}
+    for (const d in data) {
+      if (getList.includes(d)) {
+        selected[d] = data[d]
+      } else if (defaults[d]) {
+        selected[d] = defaults[d]
+      }
+    }
+    return selected
+  }
+}
