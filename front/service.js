912279c8ce21d52869598a8040eee679ded1a708
Lakshamana <guitrompa1@gmail.com>
2019-07-01 14:03:15 -0300

    Resolve conflitos de importação

--- front/service.js
+++ front/service.js
@@ -2,7 +2,7 @@ import makeStudentServices from './students/service'
 import makeDocumentsServices from './documents/service'
 import makeSubjectsServices from './subjects/service'
 import makePendenciesServices from './pendencies/service'
-import makeDefensesServices from './pendencies/service'
+import makeDefensesServices from './defenses/service'
 
 export default function makeServices(axios) {
   return {
@@ -12,7 +12,6 @@ export default function makeServices(axios) {
     pendencies: makePendenciesServices(axios),
     defenses: makeDefensesServices(axios),
     solicitations: null,
-    users: null,
-    defenses: null
+    users: null
   }
 }
