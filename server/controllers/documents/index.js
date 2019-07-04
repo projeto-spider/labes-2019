44ce1261a9c5b79a922a8e9de7b8f7518654fd34
Daniel Santana <daniel.santana.1661@gmail.com>
2019-07-03 21:10:26 -0300

    Atualização dos index.js

--- server/controllers/documents/index.js
+++ server/controllers/documents/index.js
@@ -3,6 +3,5 @@ const List = require('./List')
 const Upload = require('./Upload')
 const View = require('./View')
 const Delete = require('./Delete')
-const TccGenerate = require('./TccGenerate')
 
-module.exports = { Show, List, Upload, View, Delete, TccGenerate }
+module.exports = { Show, List, Upload, View, Delete }
