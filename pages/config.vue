fa8efdcd0d8b452ce51836788ebc8aaa59c237bb
urielfcampos <urielfcampos95@gmail.com>
2019-07-06 20:14:54 -0300

    Ajustes na pagina de config

--- pages/config.vue
+++ pages/config.vue
@@ -2,31 +2,45 @@
   <div class="container">
     <p class="title has-text-centered">Configurações</p>
     <div class="box">
-      <form @submit.prevent="handleSubmit">
-        <b-field label="Nome do Diretor">
-          <b-input v-model="directorName"></b-input>
-        </b-field>
-        <b-field label="Nome do Diretor do instituto">
-          <b-input v-model="departamentDirectorName"></b-input>
-        </b-field>
-        <b-field label="Nome do Reitor">
-          <b-input v-model="deanName"></b-input>
-        </b-field>
-        <b-field label="Período" message="formato : xxxx.[1-4]">
-          <b-input
-            v-model="term"
-            message="something"
-            pattern="[0-9]{4,4}\.[1-4]"
-          ></b-input>
-        </b-field>
-        <button class="button is-primary">Atualizar</button>
-      </form>
+      <label class="label">Nome do Diretor</label>
+      <b-field>
+        <b-input v-model="directorName" expanded></b-input>
+        <button expanded class="button is-primary" @click="submitDirectorName">
+          Atualizar
+        </button>
+      </b-field>
+      <label class="label">Nome do Diretor do instituto</label>
+      <b-field>
+        <b-input v-model="departamentDirectorName" expanded></b-input>
+        <button
+          class="button is-primary"
+          @click="submitDepartamentDirectorName"
+        >
+          Atualizar
+        </button>
+      </b-field>
+      <label class="label">Nome do Reitor</label>
+      <b-field>
+        <b-input v-model="deanName" expanded></b-input>
+        <button class="button is-primary" @click="submitDeanName">
+          Atualizar
+        </button>
+      </b-field>
+      <label class="label">Período</label>
+      <b-field>
+        <b-input
+          v-model="term"
+          expanded
+          message="something"
+          pattern="[0-9]{4,4}\.[1-4]"
+        ></b-input>
+        <button class="button is-primary" @click="submitTerm">Atualizar</button>
+      </b-field>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 export default {
   name: 'Config',
   head() {
@@ -39,34 +53,135 @@ export default {
       directorName: '',
       departamentDirectorName: '',
       deanName: '',
-      term: ''
+      term: '',
+      currentDirectorName: '',
+      currentDepartamentDirectorName: '',
+      currentTerm: '',
+      currentDeanName: ''
     }
   },
-  computed: {
-    ...mapGetters({
-      currentDirectorName: 'config/getDirectorName',
-      currentDepartamentDirectorName: 'config/getDepartamenteDirectorName',
-      currentDeanName: 'config/getDeanName',
-      currentTerm: 'config/getTerm'
-    })
-  },
   created() {
-    this.$store.dispatch('config/configTerm')
-    this.$store.dispatch('config/configDepartamentDirector')
-    this.$store.dispatch('config/configDeanName')
-    this.$store.dispatch('config/configDepartamentDirector')
+    this.$services.config.fetch('currentTerm').then(res => {
+      this.currentTerm = res.data.value
+      this.term = res.data.value
+    })
+    this.$services.config.fetch('facultyDirectorName').then(res => {
+      this.currentDirectorName = res.data.value
+      this.directorName = res.data.value
+    })
+    this.$services.config.fetch('deanName').then(res => {
+      this.currentDeanName = res.data.value
+      this.deanName = res.data.value
+    })
+    this.$services.config.fetch('departamentDirector').then(res => {
+      this.currentDepartamentDirectorName = res.data.value
+      this.departamentDirectorName = res.data.value
+    })
   },
   methods: {
     getConfigValues() {
       this.directorName = this.currentDirectorName
       this.departamentDirectorName = this.currentDepartamentDirectorName
       this.deanName = this.currentDeanName
-      this.currentTerm = this.currentTerm
+      this.term = this.currentTerm
     },
-    handleSubmit() {
-      this.$services.config
-        .updateTerm({ currentTerm: this.term })
-        .then(this.term)
+    submitTerm() {
+      if (this.term !== this.currentTerm) {
+        this.$services.config
+          .updateTerm(this.term)
+          .then(res => {
+            this.term = res.data.value
+            this.$toast.open({
+              type: 'is-success',
+              message: 'Atualizado com sucesso.'
+            })
+          })
+          .catch(e => {
+            this.$toast.open({
+              type: 'is-danger',
+              message: 'Falha ao atualizar a configuração'
+            })
+          })
+      } else {
+        this.$toast.open({
+          type: 'is-warning',
+          message: 'valor não foi alterado por serem iguais.'
+        })
+      }
+    },
+    submitDirectorName() {
+      if (this.directorName !== this.currentDirectorName) {
+        this.$services.config
+          .updateFacultyDirectorName(this.directorName)
+          .then(res => {
+            this.directorName = res.data.value
+            this.$toast.open({
+              type: 'is-success',
+              message: 'Atualizado com sucesso.'
+            })
+          })
+          .catch(e => {
+            this.$toast.open({
+              type: 'is-danger',
+              message: 'Falha ao atualizar a configuração'
+            })
+          })
+      } else {
+        this.$toast.open({
+          type: 'is-warning',
+          message: 'valor não foi alterado por serem iguais.'
+        })
+      }
+    },
+    submitDepartamentDirectorName() {
+      if (
+        this.departamentDirectorName !== this.currentDepartamentDirectorName
+      ) {
+        this.$services.config
+          .updateDepartamentDirector(this.departamentDirectorName)
+          .then(res => {
+            this.departamentDirectorName = res.data.value
+            this.$toast.open({
+              type: 'is-success',
+              message: 'Atualizado com sucesso.'
+            })
+          })
+          .catch(e => {
+            this.$toast.open({
+              type: 'is-danger',
+              message: 'Falha ao atualizar a configuração'
+            })
+          })
+      } else {
+        this.$toast.open({
+          type: 'is-warning',
+          message: 'valor não foi alterado por serem iguais.'
+        })
+      }
+    },
+    submitDeanName() {
+      if (this.deanName !== this.currentDeanName) {
+        this.$services.config
+          .updateDeanName(this.deanName)
+          .then(res => {
+            this.deanName = res.data.value
+            this.$toast.open({
+              type: 'is-success',
+              message: 'Atualizado com sucesso.'
+            })
+          })
+          .catch(e => {
+            this.$toast.open({
+              type: 'is-danger',
+              message: 'Falha ao atualizar a configuração'
+            })
+          })
+      } else {
+        this.$toast.open({
+          type: 'is-warning',
+          message: 'valor não foi alterado por serem iguais.'
+        })
+      }
     }
   }
 }
