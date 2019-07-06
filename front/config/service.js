6a7ff8f57e2f21bb896e57b6a2c0622ae3be461f
urielfcampos <urielfcampos95@gmail.com>
2019-07-06 20:14:54 -0300

    Ajustes na pagina de config

--- front/config/service.js
+++ front/config/service.js
@@ -1,17 +1,4 @@
-import selector from '@/front/selector'
 export default function makeConfigServices(axios) {
-  const paramList = [
-    'facultyDirectorName',
-    'deanName',
-    'departamentDirector',
-    'currentTerm'
-  ]
-  /* model = {
-    name: 'Josivaldo',
-    title: 'doutor',
-    member: 'facomp',
-    isTeacher: true
-  } */
   const url = '/api/settings'
   return {
     fetch(key) {
@@ -22,15 +9,15 @@ export default function makeConfigServices(axios) {
       return axios.put(`${url}/currentTerm`, payload)
     },
     updateFacultyDirectorName(value) {
-      const payload = selector(value, paramList)
+      const payload = { value }
       return axios.put(`${url}/facultyDirectorName`, payload)
     },
     updateDeanName(value) {
-      const payload = selector(value, paramList)
+      const payload = { value }
       return axios.put(`${url}/deanName`, payload)
     },
     updateDepartamentDirector(value) {
-      const payload = selector(value, paramList)
+      const payload = { value }
       return axios.put(`${url}/departamentDirector`, payload)
     }
   }
