1b9ef775a94f7c2683cd32e514877bc640305aeb
urielfcampos <urielfcampos95@gmail.com>
2019-07-06 23:37:34 -0300

    Adiciona condicional para gerar certificado e pagina principal

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,43 @@
 <template>
-  <section class="container"></section>
+  <section class="container has-text-centered">
+    <img src="../assets/images/UFPA.png" width="300" height="300" />
+    <p class="title">Implementado pela Turma de 2016</p>
+    <br />
+    <p class="subtitle">Equipe de Implementação</p>
+    <p>URIEL FERREIRA CAMPOS</p>
+    <p>JOÃO DE DEUS FERREIRA FILHO</p>
+    <p>LUCAS GABRIEL DE SOUZA</p>
+    <p>GUILHERME SMETHURST ALBUQUERQUE</p>
+    <p>DANIEL SANTOS SANTANA</p>
+    <p>JAIRO NASCIMENTO DE SOUSA FILHO</p>
+    <p>TEOIAN GIOVANE COSTA QUARESMA</p>
+    <p>ANDRÉ MIRANDA PINHEIRO</p>
+    <br />
+    <p class="subtitle">Equipe de Gerência</p>
+    <p>GABRIEL BELTRAO DE ABREU</p>
+    <p>JOÃO PEDRO MOREIRA MORAES</p>
+    <p>KELLY DO SOCORRO SILVA DA COSTA</p>
+    <br />
+    <p class="subtitle">Equipe de Arquitetura</p>
+    <p>ABNER CARDOSO DA SILVA</p>
+    <p>ANDRE MIRANDA PINHEIRO</p>
+    <p>ERLON FONSECA PINHEIRO</p>
+    <p>FELIPE GUSMAO ARAUJO</p>
+    <p>JOSIANE DE OLIVEIRA COELHO</p>
+    <br />
+    <p class="subtitle">Equipe de Análise de requisitos</p>
+    <p>AUGUSTO FARIAS CRAVO</p>
+    <p>CAIO SHIMADA RABELLO</p>
+    <p>DANIELA KARINE MARAVALHO AVELAR</p>
+    <p>IAN CAMPOS BEZERRA REGO</p>
+    <p>JOÃO MARCELO SILVA DE OLIVEIRA</p>
+    <br />
+    <p class="subtitle">Equipe de Teste</p>
+    <p>RAFAEL NASCIMENTO DE SOUSA</p>
+    <p>ANA ISABELA MANITO RAMOS</p>
+    <p>LUCAS FELIPE FERRARO CARDOSO</p>
+    <p>CARLOS HENRIQUE RIBEIRO CARDOSO</p>
+  </section>
 </template>
 
 <script>
@@ -15,13 +53,10 @@ export default {
 
 <style scoped>
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
   color: #35495e;
-  letter-spacing: 1px;
 }
 
 .subtitle {
