45f4da195511da1740bddcd348a06e43a1782c19
urielfcampos <urielfcampos95@gmail.com>
2019-07-06 21:47:49 -0300

    ajustes no teste

--- test/front/login.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Login from '@/pages/Login.vue'
-
-describe('ImportStudents', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Login)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
-})
