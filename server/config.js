7b7acd217c0008a31408b474817b8cd692beb417
lubien <lubien1996@gmail.com>
2019-07-07 00:10:32 -0300

    Corrige o setup do dotenv

--- server/config.js
+++ server/config.js
@@ -1,10 +1,17 @@
-const dotent = require('dotenv')
+const path = require('path')
+const dotenv = require('dotenv')
 
 const env = process.env.NODE_ENV || 'development'
 
 const silent = env !== 'production'
 
-dotent.config({ silent })
+const envSetup = dotenv.config({
+  path: path.join(__dirname, '../.env')
+})
+
+if (envSetup.error && !silent) {
+  throw envSetup.error
+}
 
 const {
   HOST = '0.0.0.0',
