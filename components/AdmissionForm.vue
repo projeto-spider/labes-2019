ad105dbf1db63a21cd733505426609ab57b504ef
Lakshamana <guitrompa1@gmail.com>
2019-07-09 23:54:23 -0300

    refatora componente de validação

--- components/AdmissionForm.vue
+++ components/AdmissionForm.vue
@@ -4,102 +4,95 @@
       <div class="card">
         <div class="card-content">
           <div class="column">
-            <b-field
-              label="Nome"
-              :message="{
-                'Campo obrigatório': !dirty.name,
-                [validName ? 'ok' : `Mínimo de 9 caracteres`]: dirty.name
-              }"
-              :type="{
-                [validName ? 'is-success' : 'is-danger']: dirty.name
-              }"
-            >
-              <b-input
-                v-model="name"
-                type="text"
-                @blur="dirty.name = true"
-              ></b-input>
-            </b-field>
-
-            <b-field
-              label="Email"
-              :message="{
-                'Campo obrigatório': !dirty.email,
-                [validEmail ? 'ok' : 'Digite um endereço de email']: dirty.email
-              }"
-              :type="{
-                [validEmail ? 'is-success' : 'is-danger']: dirty.email
-              }"
-            >
-              <b-input
-                v-model="email"
-                type="text"
-                @blur="dirty.email = true"
-              ></b-input>
-            </b-field>
-
-            <b-field
-              label="Matrícula"
-              :message="{
-                'Campo opcional': !dirty.registration,
-                [validRegistration
-                  ? 'ok'
-                  : 'Deve ter 12 caracteres numéricos']: dirty.registration
-              }"
-              :type="{
-                [validRegistration
-                  ? 'is-success'
-                  : 'is-danger']: dirty.registration
-              }"
-            >
-              <b-input
-                v-model="registration"
-                type="text"
-                @blur="dirty.registration = true"
-              ></b-input>
-            </b-field>
-            <div v-if="currentRouteName !== 'concludingAdmission'">
-              <b-field
-                label="Curso"
-                :message="{
-                  'Campo Obrigatório': !dirty.registration
-                }"
+            <form @submit.prevent="sendSolicitation">
+              <input-validation
+                ref="nameIpt"
+                input-label="Nome"
+                default-message="Campo obrigatório"
+                :invalid-message="['Mínimo de 9 caracteres']"
+                valid-message="Ok"
+                :valid="validName"
               >
-              </b-field>
-              <div v-for="c in $options.courses" :key="c.id">
-                <div class="field">
-                  <b-radio v-model="course" :native-value="c.id">
-                    {{ c.name }}
-                  </b-radio>
+                <b-input
+                  v-model="name"
+                  type="text"
+                  @blur="onBlur('nameIpt')"
+                ></b-input>
+              </input-validation>
+
+              <input-validation
+                ref="emailIpt"
+                input-label="Email"
+                default-message="Campo obrigatório"
+                :invalid-message="['Digite um endereço de email válido']"
+                valid-message="Ok"
+                :valid="validEmail"
+              >
+                <b-input
+                  v-model="email"
+                  type="text"
+                  @blur="onBlur('emailIpt')"
+                ></b-input>
+              </input-validation>
+
+              <input-validation
+                ref="regIpt"
+                input-label="Matrícula"
+                default-message="Campo opcional"
+                :invalid-message="['Deve ter 12 caracteres numéricos']"
+                valid-message="Ok"
+                :valid="validRegistration"
+              >
+                <b-input
+                  v-model="registration"
+                  type="text"
+                  @blur="onBlur('regIpt')"
+                ></b-input>
+              </input-validation>
+
+              <div v-if="currentRouteName !== 'concludingAdmission'">
+                <b-field
+                  label="Curso"
+                  :message="{
+                    'Campo Obrigatório': !dirty.registration
+                  }"
+                >
+                </b-field>
+                <div v-for="c in $options.courses" :key="c.id">
+                  <div class="field">
+                    <b-radio v-model="course" :native-value="c.id">
+                      {{ c.name }}
+                    </b-radio>
+                  </div>
+                </div>
+                <br v-if="currentRouteName !== 'concludingAdmission'" />
+                <b-field
+                  v-if="currentRouteName !== 'concludingAdmission'"
+                  label="Forma de Ingresso"
+                  :message="{
+                    'Campo Obrigatório': !dirty.registration
+                  }"
+                >
+                </b-field>
+                <div v-for="at in $options.admissionTypes" :key="at.id">
+                  <div class="field">
+                    <b-radio v-model="admissionType" :native-value="at.id">
+                      {{ at.name }}
+                    </b-radio>
+                  </div>
                 </div>
               </div>
-              <br v-if="currentRouteName !== 'concludingAdmission'" />
-              <b-field
-                v-if="currentRouteName !== 'concludingAdmission'"
-                label="Forma de Ingresso"
-                :message="{
-                  'Campo Obrigatório': !dirty.registration
-                }"
-              >
-              </b-field>
-              <div v-for="at in $options.admissionTypes" :key="at.id">
-                <div class="field">
-                  <b-radio v-model="admissionType" :native-value="at.id">
-                    {{ at.name }}
-                  </b-radio>
-                </div>
-              </div>
-            </div>
 
-            <div class="level-right">
-              <b-button
-                class="is-primary level-item"
-                :disabled="!enabledButton"
-                @click.prevent="sendSolicitation"
-              >
-                Enviar
-              </b-button>
-            </div>
+              <div class="level-right">
+                <b-button
+                  class="is-primary level-item"
+                  :disabled="!enabledButton"
+                  native-type="submit"
+                >
+                  Enviar
+                </b-button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
@@ -109,9 +102,13 @@
 
 <script>
 import { errorsHandler } from '@/components/mixins/errors'
+import InputValidation from '@/components/InputValidation'
 
 export default {
   name: 'AdmissionForm',
+  components: {
+    InputValidation
+  },
   mixins: [errorsHandler],
   props: {
     mailingList: {
@@ -143,7 +140,7 @@ export default {
   }),
   computed: {
     validName() {
-      return this.dirty.name && this.name.length >= 9
+      return this.name.length >= 9
     },
 
     validEmail() {
@@ -164,6 +161,17 @@ export default {
   },
 
   methods: {
+    onBlur(refName) {
+      this.$refs[refName].dirty = true
+    },
+
+    clearRefsDirty() {
+      const refs = Object.keys(this.$refs)
+      for (const r of refs) {
+        this.$refs[r].dirty = false
+      }
+    },
+
     sendSolicitation() {
       this.$services.solicitations
         .create({
@@ -184,9 +192,7 @@ export default {
           this.registration = ''
           this.course = 'cbcc'
           this.admissionType = 'psufpa'
-          this.dirty.name = ''
-          this.dirty.email = ''
-          this.dirty.registration = ''
+          this.clearRefsDirty()
         })
         .catch(error => this.openErrorNotification(error.response.data.code))
     }
