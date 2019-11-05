6438d5befda08eda70d6bcb73e49c4c423d302a8
lubien <lubien1996@gmail.com>
2019-11-05 11:43:07 -0300

    Prepare seeds for production

--- test/use-seeds-data/documents.js
+++ test/use-seeds-data/documents.js
@@ -1,17 +1,20 @@
-module.exports = [
-  {
-    studentId: 1,
-    type: 1,
-    url: 'url/file1'
-  },
-  {
-    studentId: 1,
-    type: 2,
-    url: 'url/file2'
-  },
-  {
-    studentId: 2,
-    type: 3,
-    url: 'url/file3'
-  }
-]
+module.exports =
+  process.env.NODE_ENV === 'production'
+    ? []
+    : [
+        {
+          studentId: 1,
+          type: 1,
+          url: 'url/file1'
+        },
+        {
+          studentId: 1,
+          type: 2,
+          url: 'url/file2'
+        },
+        {
+          studentId: 2,
+          type: 3,
+          url: 'url/file3'
+        }
+      ]
--- test/use-seeds-data/users.js
+++ test/use-seeds-data/users.js
@@ -1,23 +1,34 @@
-module.exports = [
-  {
-    username: 'admin',
-    fullName: 'Administrador',
-    password: 'admin',
-    role: 'admin',
-    email: 'admin@domain.com'
-  },
-  {
-    username: 'user',
-    password: 'user',
-    fullName: 'Usuário Qualquer',
-    role: 'admin',
-    email: 'user@domain.com'
-  },
-  {
-    username: 'teacher',
-    password: 'teacher',
-    fullName: 'Professor',
-    role: 'teacher',
-    email: 'teacher@domain.com'
-  }
-]
+module.exports =
+  process.env.NODE_ENV === 'production'
+    ? [
+        {
+          username: 'secretaria',
+          fullName: 'Secretaria',
+          password: 'secretaria',
+          role: 'admin',
+          email: 'secretaria@domain.com'
+        }
+      ]
+    : [
+        {
+          username: 'admin',
+          fullName: 'Administrador',
+          password: 'admin',
+          role: 'admin',
+          email: 'admin@domain.com'
+        },
+        {
+          username: 'user',
+          password: 'user',
+          fullName: 'Usuário Qualquer',
+          role: 'admin',
+          email: 'user@domain.com'
+        },
+        {
+          username: 'teacher',
+          password: 'teacher',
+          fullName: 'Professor',
+          role: 'teacher',
+          email: 'teacher@domain.com'
+        }
+      ]
